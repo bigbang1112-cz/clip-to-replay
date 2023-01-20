093a54853613221ca9042139314236721eba34f0
Petr Pivoňka <petrpiv1@gmail.com>
2023-01-20 22:06:30 +0100

    Add GbxToolAPI

--- ClipToReplay/ClipToReplay.cs
+++ ClipToReplay/ClipToReplay.cs
@@ -1,5 +1,21 @@
-﻿namespace ClipToReplay;
+﻿using GBX.NET.Engines.Game;
+using GbxToolAPI;
 
-public class ClipToReplay
+namespace ClipToReplay;
+
+public class ClipToReplay : Tool, IHasOutput<BinFile>
 {
+    private readonly CGameCtnChallenge map;
+    private readonly CGameCtnMediaClip clip;
+
+    public ClipToReplay(CGameCtnChallenge map, CGameCtnMediaClip clip)
+    {
+        this.map = map;
+        this.clip = clip;
+    }
+
+    public BinFile Produce()
+    {
+        throw new NotImplementedException();
+    }
 }
